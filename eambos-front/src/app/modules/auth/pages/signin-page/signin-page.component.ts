b44e83afd46520fe2f9cee899a8a8efcc48abb4c
Daniel Palli <dpallicolque@frba.utn.edu.ar>
2024-01-11 12:53:26 -0300

    Angular -> Next

--- eambos-front/src/app/modules/auth/pages/signin-page/signin-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './signin-page.component.html',
-  styleUrls: ['./signin-page.component.css']
-})
-export class SigninPageComponent {
-
-}
--- eambos-front/src/app/modules/auth/pages/signup-page/signup-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
-})
-export class SignupPageComponent {
-
-}
